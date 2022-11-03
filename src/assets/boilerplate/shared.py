9fc5bbd71939eff6a68e23ecd8225d770e99d514
Alex Harrison <alexjharrison@gmail.com>
2022-11-03 18:58:20 -0400

    add formatDataset function for formatting dataset

--- src/assets/boilerplate/shared.py
+++ src/assets/boilerplate/shared.py
@@ -2,11 +2,16 @@ import pathlib
 from os import getenv
 
 folder = str(pathlib.Path(__file__).parent.absolute())
-file = "/dataset-short.txt" if getenv("USE_SHORT") else "/dataset.txt"
+file = "/dataset.txt" if getenv("USE_SHORT") else "/dataset-short.txt"
 
 
-def getDataset() -> "list[str]":
+# Input is an list of each line of the text file cast as a string
+# Change the output to something easier for you to process
+def _formatDataset(dataset: "list[str]"):
+    return dataset
+
+
+def getDataset():
     with open(folder + file, 'r') as d:
         lines = [s for s in d.read().splitlines() if s.strip()]
-        print(lines)
-        return lines
+        return _formatDataset(lines)
